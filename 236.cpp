b4ced6a139982992d37b9550ec46765f2d6e5c50
yeyuxiaoming <zhipengyu007@foxmai.com>
2020-12-30 19:34:51 +0800

    letcode236

---
+++ 236.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ /*236给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+            return NULL;
+        if(root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left == NULL) {
+            return right;
+        }
+        if(right == NULL) {
+            return left;
+        }
+        if(left && right) {
+            return root;
+        }
+        return NULL;
+    }
+};
